0174f90a64783110189686e11fa0d62949d85288
Prateek Sharma <91019339+PrateekSharma007@users.noreply.github.com>
2023-04-02 05:27:43 +0530

    new changes navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,29 @@
 import React from 'react';
 import Navbar from './components/Navbar';
 import {BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Account from './pages/Account';
+import Signin from './pages/Signin';
+
+
+
+
+
+
+
 
 
 function App() {
   return (
     <Router>
-    <div className="pt-20">
+    <div className="bg bg-black h-screen">
       <Navbar />
       <Routes>
         {/* <Route path="/" element={<Home />}/> */}
         {/* <Route path="/about" element={<About />} /> */}
+        <Route path = "/" element = {<Home/>} />
+        <Route path = "/account" element = {<Account/>} />
+        <Route path = "/signin" element = {<Signin/>} />
       </Routes>
     </div>
     </Router>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,38 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { GiHamburgerMenu } from "react-icons/gi"
+
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className="flex justify-between items-end text-white h-24 max-w-[1240px] mx-auto px-4">
+      <div className="left">
+        <h1 className="text-green-500 text-2xl">WEBSITE.</h1>
+      </div>
+      <div className="right ">
+        <ul className="flex justify-between items-center gap-5 hidden">
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/account">Account</Link></li>
+          <li><Link to="/signin">Signin</Link></li>
+          <button class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
+            Create Account
+          </button>
+        </ul>
+        <div>
+          <GiHamburgerMenu size={20} />
+        </div>
+        <div className="fixed left-0 top-0   w-[60%] border-r border-r-gray-900">
+          <ul className = "p-5 mt-28 h-[100vh] text-white">
+            <li className='border-b border-gray-500 pb-4'><Link to="/">Home</Link></li>
+            <li className='border-b border-gray-500 pb-4'><Link to="/account">Account</Link></li>
+            <li className='border-b border-gray-500 pb-4'><Link to="/signin">Signin</Link></li>
+          </ul>
+        </div>
+
+      </div>
+    </div>
   )
 }
+
 
 export default Navbar
